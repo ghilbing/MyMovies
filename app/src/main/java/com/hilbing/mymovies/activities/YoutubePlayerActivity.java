1da95b9435565d69dbab8fb8e4b7c01dbcf53724
ghilbing <ghilbing@gmail.com>
2019-06-18 09:00:50 -0700

    Youtube android api not working

---
+++ app/src/main/java/com/hilbing/mymovies/activities/YoutubePlayerActivity.java
@@ -0,0 +1,55 @@
+package com.hilbing.mymovies.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+import com.hilbing.mymovies.BuildConfig;
+import com.hilbing.mymovies.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class YoutubePlayerActivity extends YouTubeBaseActivity {
+
+    @BindView(R.id.youtube_player)
+    YouTubePlayerView youTubePlayer;
+
+    public static final String EXTRA_KEY = "key";
+    private String key = "";
+
+    @Override
+    protected void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        setContentView(R.layout.activity_youtube_player);
+
+        ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+        key = intent.getStringExtra(EXTRA_KEY);
+
+        if(BuildConfig.YouTubeKey.isEmpty()){
+            Toast.makeText(getApplicationContext(), getResources().getString(R.string.get_api_key_youtube), Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        youTubePlayer.initialize((BuildConfig.YouTubeKey), new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.cueVideo(key);
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        });
+
+
+    }
+}
--- app/src/main/java/com/hilbing/mymovies/adapter/TrailerAdapter.java
+++ app/src/main/java/com/hilbing/mymovies/adapter/TrailerAdapter.java
@@ -5,13 +5,16 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.hilbing.mymovies.MyApplication;
 import com.hilbing.mymovies.R;
+import com.hilbing.mymovies.activities.YoutubePlayerActivity;
 import com.hilbing.mymovies.model.Trailer;
 
 import java.util.List;
@@ -20,6 +23,8 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class TrailerAdapter extends RecyclerView.Adapter<TrailerAdapter.TrailerViewHolder> {
+
+    private static final String TAG = TrailerAdapter.class.getSimpleName();
 
     private Context mContext;
     private List<Trailer> mTrailerList;
@@ -57,19 +62,33 @@ public class TrailerAdapter extends RecyclerView.Adapter<TrailerAdapter.TrailerV
             super(itemView);
             ButterKnife.bind(this, itemView);
 
-            itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    int position = getAdapterPosition();
-                    if (position != RecyclerView.NO_POSITION){
-                        String videoId = mTrailerList.get(position).getKey();
-                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("vnd.youtube:" + videoId));
-                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                        intent.putExtra("VIDEO_ID", videoId);
-                        mContext.startActivity(intent);
-                    }
-                }
-            });
+            Toast.makeText(mContext, String.valueOf(getAdapterPosition()), Toast.LENGTH_LONG).show();
+            int index = getAdapterPosition();
+
+            Uri uriVideo = mTrailerList.get(index).getYoutubeURL();
+            String key = mTrailerList.get(getAdapterPosition()).getKey();
+            Log.d(TAG, "Play URL: " + uriVideo);
+
+            Intent intent = new Intent(mContext, YoutubePlayerActivity.class);
+            intent.putExtra(YoutubePlayerActivity.EXTRA_KEY, key);
+            mContext.startActivity(intent);
+
+
+
+
+//            itemView.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    int position = getAdapterPosition();
+//                    if (position != RecyclerView.NO_POSITION){
+//                        String videoId = mTrailerList.get(position).getKey();
+//                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("vnd.youtube:" + videoId));
+//                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//                        intent.putExtra("VIDEO_ID", videoId);
+//                        mContext.startActivity(intent);
+//                    }
+//                }
+//            });
         }
     }
 }
